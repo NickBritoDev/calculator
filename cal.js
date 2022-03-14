ec3317e7e3708767e31ee7ee494148ce4b8e562d
Brito <101473345+NickBritoDev@users.noreply.github.com>
2022-03-14 15:34:38 -0300

    Add files via upload

---
+++ cal.js
@@ -0,0 +1,13 @@
+var valor;
+function botao (num) {
+  valor = document.calc.visor.value += num;
+}
+
+function reset (){
+  document.calc.visor.value = ' ';
+}
+
+function calcula () {
+  resultado= eval (valor);
+document.calc.visor.value = resultado.toLocaleString('pt') ;
+}
